790412f8b3e038a9b01304fa2dd802f06187577e
mandar <mnikam02@gmail.com>
2018-01-09 18:18:13 +0530

    Update db.js

--- app/config/db.js
+++ app/config/db.js
@@ -3,7 +3,7 @@ var db;
 
 module.exports.connect = function(cb){
     if(mongoose.connection.readyState != 1 ||  mongoose.connection.readyState != 2){
-    	mongoose.connect('mongodb://52.23.205.231/cars');
+    	mongoose.connect('mongodb://34.239.27.78/cars');
 	    db = mongoose.connection;
 	    db.on('error', function(){ cb(false, "Error in database connection")} );
 	    cb(db);	
