64a6eefeef8bd57b678135c4c0068cd89f8243c0
tusharncuelogic <tushar.nikam@cuelogic.co.in>
2018-01-31 19:15:48 +0530

    Fixed mongodb port issue

--- app/config/db.js
+++ app/config/db.js
@@ -5,9 +5,10 @@ const nconf = require("nconf").argv();
 module.exports.connect = function(cb){
     var mongoHost =  (nconf.get('MONGO_HOST') &&  nconf.get('MONGO_HOST') != '') ? nconf.get('MONGO_HOST') : "localhost";
     var mongoPort =  (nconf.get('MONGO_PORT') &&  nconf.get('MONGO_PORT') != '') ? nconf.get('MONGO_PORT') : 27017;   
-    
+    var mongoConnectUrl = 'mongodb://'+mongoHost+':'+mongoPort+'/cars';
+    console.log(mongoConnectUrl, "mongoConnectUrl");
     if(mongoose.connection.readyState != 1 ||  mongoose.connection.readyState != 2){
-    	mongoose.connect('mongodb://'+mongoHost+':'+mongoPort+'/cars');
+    	mongoose.connect(mongoConnectUrl);
 	    db = mongoose.connection;
 	    db.on('error', function(){ cb("Error in database connection")} );
 	    cb(false,db);
